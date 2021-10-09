e164827cb80bae4b730a0cdc089ede003f874e19
Jeaniel Osorno <jeanielosorno@gmail.com>
2021-10-09 12:43:58 -0500

    Commit 5. Creacion ventanas intermedias

--- MainRoot.py
+++ MainRoot.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from MyFrames import first_window, new_question
+from MyFrames import first_window, new_question, between_right_answer
 import random
 
 
@@ -26,34 +26,53 @@ for i in range(5):
 
 firstWindow = first_window()
 
+
+def between_answer(change_category, points):
+
+    between = between_right_answer(change_category, points)
+
+    if between.Surrender == True:
+        return 1
+    elif between.Retreat == True:
+        return 2
+    elif between.Next_question == True:
+        return 3
+
+
 if (firstWindow.closed == True):
     
     Category = 1
+    aux_category = Category
+    optionReturned = None
     question_count = 0
     question_container = list()
     back_tail = 0
     front_tail = 5
     game_over = False
+    points_until_now = 0
 
     for round in range(5):        
+        
         if (game_over == True):
             break
+        
         containerAux = questionsAux[back_tail:front_tail]
-        print(containerAux)
+        
         for questionNumber in range(5):
             
             if (game_over == True):
                 break
             
             while True:
+
+                if (game_over == True):
+                    break
+
                 aux = random.choice(containerAux)
                 if aux not in question_container:
                     question_container.append(aux)
                     question_count += 1
                     if ( len(question_container) % 5 ) == 0:
-                        back_tail += 5
-                        front_tail += 5
-                        Category += 1
                         break
                     break
                 else:
@@ -61,15 +80,51 @@ if (firstWindow.closed == True):
             
             try:
                 if (question.wants_to_exit == True):
+                    game_over = question.wants_to_exit
                     break
                 else:
                     question = new_question(aux, Category, int(question_count))
                     game_over = question.game_over
-                    print("La respuesta correcta es", question.right_answer)
+                    print("!!! La respuesta correcta es", question.right_answer)
             except:
                 question = new_question(aux, Category, int(question_count))
                 game_over = question.game_over
-                print("La respuesta correcta es", question.right_answer)
+                print("*** La respuesta correcta es", question.right_answer)
+            
+            if ( len(question_container) % 5) == 0:
+                back_tail += 5
+                front_tail += 5
+                Category += 1
+
+            if (question.points == 0):
+                #Construir ventana de fin del juego
+                game_over = True
+                print("Fin del juego")
+                break
+            else:
+                points_until_now += 4
+                game_over = False
+                print("Puntos hasta ahora:", points_until_now)
+
+                if (aux_category != Category):
+                    aux_category = Category
+                    optionReturned = between_answer(1, points_until_now)
+                else:
+                    optionReturned = between_answer(0, points_until_now)
+            
+            if (optionReturned == 1):
+                game_over = True
+                #Ventana de fin del juego por rendirse
+                print("Opción 1")
+                break
+            elif (optionReturned == 2):
+                game_over = True
+                #Ventana de fin del juego por retirarse
+                print("Opción 2")
+                break
+            elif (optionReturned == 3):
+                game_over = False
+                print("Opción  3")
             
         continue
     
--- MyFrames.py
+++ MyFrames.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import random
+from tkinter import font
 
 class new_question:
 
@@ -7,6 +8,7 @@ class new_question:
     game_over = False
     wants_to_exit = False
     right_answer=0
+    points = 4
 
     def __init__(self, question, category, counter):
         self.Question = question
@@ -39,6 +41,7 @@ class new_question:
 
         def Exit_window():
             self.wants_to_exit = True
+            self.points = 0
             root.destroy()
         MyButtonClose = Button(MyFrame, text="Salir", padx=10, pady=5, command=Exit_window)
         MyButtonClose.grid(row=6, column=3)
@@ -47,10 +50,12 @@ class new_question:
             if (varOption.get() != self.right_answer):
                 print("Perdió!")
                 self.game_over = True
+                self.points = 0
                 root.destroy()
             else:
                 print("Acertó!")
                 self.game_over = False
+                self.points = 4
                 root.destroy()
         MyButtonContinue = Button(MyFrame, text="Responder", padx=10, pady=5, command=lambda:answer_question())
         MyButtonContinue.grid(row=6, column=4)
@@ -97,3 +102,96 @@ class first_window:
 
         root.mainloop()
 
+class between_right_answer:
+
+    Retreat = False
+    Next_question = False
+    Surrender = False
+
+    def __init__(self, change_category, points):
+        
+
+
+        root = Tk()
+        root.title("Sofka U - Acertaste!")
+        root.iconbitmap("Icons/SofkaU_Icon.ico")
+        root.geometry("650x350")
+        MyFrame = Frame(root, padx=10, pady=10)
+        MyFrame.pack()
+
+        LabelOne = Label(MyFrame, text="Felicidades, ha contestado correctamente!", font=30)
+        LabelOne.grid(row=2, column=0, columnspan=3)
+
+        def retreat_button():
+            self.Retreat = True
+            root.destroy()
+            
+        def next_question():
+            self.Next_question = True
+            root.destroy()
+
+        def surrender():
+            self.Surrender = True
+            root.destroy()
+
+        if (change_category == 1):
+            LabelTwo = Label(MyFrame, text="Puede retirarse ahora conservando su total de puntos")
+            LabelThree = Label(MyFrame, text="O puede continuar a la siguiente pregunta")
+            ButtonOne = Button(MyFrame, text="Retirarse", padx=10, pady=10, command=retreat_button)
+            ButtonOne.grid(row=6, column=2)
+            ButtonTwo = Button(MyFrame, text="Siguiente", padx=10, pady=10, command=lambda:next_question())
+            ButtonTwo.grid(row=6, column=3)
+        else:
+            LabelTwo = Label(MyFrame, text="Puede retirarse sin conservar sus puntos")
+            LabelThree = Label(MyFrame, text="O puede continuar a la siguiente pregunta")
+            ButtonOne = Button(MyFrame, text="Rendirse", command=surrender)
+            ButtonOne.grid(row=6, column=2)
+            ButtonTwo = Button(MyFrame, text="Siguiente", command=lambda:next_question())
+            ButtonTwo.grid(row=6, column=3)
+        LabelTwo.grid(row=3, column=2)
+        LabelThree.grid(row=4, column=2)
+        LabelFour = Label(MyFrame, text=f"Puntos: {points}", font=20)
+        LabelFour.grid(row=5, column=1)
+
+
+
+
+
+        root.mainloop()
+
+class game_over:
+
+
+    def __init__(self, end, points, answered_questions):
+        root = Tk()
+        root.title("Sofka U - Fin del juego")
+        root.iconbitmap("Icons/SofkaU_Icon.ico")
+        root.geometry("650x350")
+        MyFrame = Frame(root, padx=10, pady=10)
+        MyFrame.pack()
+
+        def close_window():
+            root.destroy()
+
+        if (end == 1):
+            #Cuando se equivoca en una pregunta
+            print("Game Over")
+
+            LabelOne = Label(MyFrame, text="Has perdido el juego :(", font=30)
+            LabelTwo = Label(MyFrame, text=f"Total de puntos: {points}")
+
+        elif (end == 2):
+            #Cuando completa las 25 preguntas
+            print("Ganador indiscutible")
+            LabelOne = Label(MyFrame, text="Has contestado correctamente las 25 preguntas :D", font=30)
+            LabelTwo = Label(MyFrame, text=f"Total de puntos: {points}")
+        elif (end == 3):
+            #Cuando se retira
+            LabelOne = Label(MyFrame, text=f"Has contestado correctamente {answered_questions} preguntas :D", font=30)
+            LabelTwo = Label(MyFrame, text=f"Total de puntos: {points}")
+        
+        LabelOne.grid(row=1, column=1, columnspan=2)
+        LabelTwo.grid(row=3, column=1, columnspan=2)
+
+        ButtonAccept = Button(MyFrame, text="Aceptar", padx=10, pady=5, command=lambda:close_window())
+        ButtonAccept.grid(row=5, column=2)
