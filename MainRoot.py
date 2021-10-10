4704723d34336fdf7ce32c11ed00100fbd6e9b02
Jeaniel Osorno <jeanielosorno@gmail.com>
2021-10-09 21:16:43 -0500

    Commit 9. Comentarios y error menor

--- MainRoot.py
+++ MainRoot.py
@@ -2,89 +2,106 @@ from tkinter import *
 from MyFrames import first_window, new_question, between_right_answer, endgame, save_user
 import random
 
-
+#Abro el archivo con las preguntas almacenadas
 questions = open("Preguntas/Preguntas.txt", "r", encoding="utf-8").read()   
+#Modifico la lista para que contenga 5 elementos correspondientes a las categorías
 questions = questions.split("Categoria:")
 questions.pop(0)
 questionsAux = list()
+
+#Creo una lista auxiliar que contendrá los mismos 5 elementos correspondientes a las categorías
 for element in range(0, 5):
     questionsAux.append(questions[element])
 questions = list()
+
+#Creo una lista de 2 dimensiones que contendrá las preguntas individualizadas.
 for category in questionsAux:
     try:
         questions.append(category.split("¿"))
     except:
         continue
+#Preparo la lista que contendrá únicamente las preguntas
 questionsAux = list()
 for i in range(5):
     for j in range(1, 6):
         try:
+            #Agrego únicamente las preguntas con sus respectivas opciones
             questionsAux.append(questions[i][j])
         except:
             continue
 
+#Llamada a la ventana inicial, con la que se verá un texto almacenado en el archivo Introduccion.txt
 firstWindow = first_window()
 
-
+#Función que, al ser llamada, crea un nuevo objeto que creará una ventana que llamé "entre respuestas"
+# Esto con el fin de darle a elegir al usuario si salir antes de pasar a la siguiente pregunta.
 def between_answer(option, points):
 
+    #El parámetro opción le indica a la clase si imprimirá un mensaje permitiendole salir conservando los puntos acumulados o no.
+    #Esto para que el jugador pueda salir con sus puntos una vez acaba la ronda
     between = between_right_answer(option, points)
 
+    #Si se rinde, significa que no pasará a la siguiente pregunta y perderá los puntos
     if between.Surrender == True:
         return 1
+    #Si se retira, significa que no pasará a la siguiente ronda y conservará los puntos
     elif between.Retreat == True:
         return 2
+    #Si así lo decide, pasará a la siguiente pregunta.
     elif between.Next_question == True:
         return 3
 
-
+#Según el valor del parámetro, guardará al usuario con sus puntos y luego mostrará el histórico, o solo mostrará el histórico.
 def saveUser(points):
+    #Si el parámetro vale cero, solo se imprimirán los usuarios.
     if (points == 0):
         user = save_user(0)
         #user.print_users()
         return None
+    #Si el parámetro vale alguno de los puntos de ronda, podrá salir y conservar sus puntos, a la vez que es registrado
     elif (points == 20) or (points == 40) or (points == 60) or (points == 80) or (points == 100):
         user = save_user(points)
         user.save_username()
         user.print_users()
+    #Si el usuario simplemente decide retirarse antes de completar una ronda, solo imprimirá el histórico.
     else:
         user = save_user(0)
         #user.print_users()
 
-
+#Dado los parámetros, puede imprimir una pantalla según si se equivocó, se retiró o se rindió.
 def end_game(points, option, ans):
     
     final_message = endgame(option, points, ans)
 
-    
-    
-    #Función para cuando se equivoca.
-    print("Fin del juego")
-
+#Si closed es verdadero, significa que el usuario decidió empezar a jugar. Sino, significa que solo presionó salir.
 if (firstWindow.closed == True):
     
-    Category = 1
-    aux_category = Category
+    Category = 1 #Categoría en la que empieza el juego.
+    aux_category = Category #Verificador de categoría.
     optionReturned = None
-    question_count = 0
-    question_container = list()
-    back_tail = 0
+    question_count = 0      #Contador de preguntas
+    question_container = list()     #Contendrá todas las preguntas hechas hasta el momento, asegurando que ninguna se repetirá.
+    back_tail = 0       #Junto con cola delantera, aseguran que las preguntas a realizar pertenezcan a una categoría específica
     front_tail = 5
-    game_over = False
-    points_until_now = 0
+    game_over = False   #Si es verdadero, el juego acaba indiscutiblemente
+    points_until_now = 0    #Acumulado de puntos.
 
+    #Ciclo de 5 iteraciones para 5 categorías
     for round in range(5):        
         
         if (game_over == True):
             break
         
+        #Toma las 5 preguntas de la categoría inicial.
         containerAux = questionsAux[back_tail:front_tail]
         
+        #Ciclo de 5 iteraciones para 5 preguntas.
         for questionNumber in range(5):
             
             if (game_over == True):
                 break
             
+            #Ciclo para asegurar que la pregunta escogida, no haya salido ya. 
             while True:
 
                 if (game_over == True):
@@ -100,66 +117,71 @@ if (firstWindow.closed == True):
                 else:
                     continue
             
+            #Si el jugador decide salir, el juego termina.
             try:
                 if (question.wants_to_exit == True):
                     game_over = question.wants_to_exit
-                    print("Quiere salir")
+                    #print("Quiere salir")
                     break
-                else:
+                else: #Si no quiere salir, se lanza la pregunta.
                     question = new_question(aux, Category, int(question_count))
                     game_over = question.game_over
-                    print("!!! La respuesta correcta es", question.right_answer)
-            except:
+                    #print("!!! La respuesta correcta es", question.right_answer)
+            except: #Si no quiere salir, se lanza la pregunta.
                 question = new_question(aux, Category, int(question_count))
                 game_over = question.game_over
-                print("*** La respuesta correcta es", question.right_answer)
+                #print("*** La respuesta correcta es", question.right_answer)
             
+            #Si ya se han hecho 5 preguntas, se pasa de categoría
             if ( len(question_container) % 5) == 0:
                 back_tail += 5
                 front_tail += 5
                 Category += 1
-
+            #Si luego de contestar la pregunta, resulta que se equivocó: fin del juego.
             if (question.points == 0):
-                #Construir ventana de fin del juego
+                #Construye ventana de fin del juego
                 game_over = True
-                print("Se ha equivocado")
+                #print("Se ha equivocado")
+                #Si se equivocó, llama a la ventana de fin de juego.
                 end_game(points_until_now, 1, question_count)
-                saveUser(points_until_now)
+                saveUser(0)
                 break
-            else:
+            else: #Si contesta bien, se pasa a evaluar.
                 points_until_now += 4
                 game_over = False
-                print("Puntos hasta ahora:", points_until_now)
+                #print("Puntos hasta ahora:", points_until_now)
 
+                #Si tiene 100 puntos, significa que ha ganado el juego.
                 if (points_until_now == 100):
                     game_over = True
                     end_game(points_until_now, 2, question_count)
                     saveUser(points_until_now)
                     break
-
+                
+                #Debido a que cada 5 preguntas se cambia de categoría, debe evaluarse que lo esté haciendo.
                 if (aux_category != Category):
                     aux_category = Category
+                    #Si cambió de categoría, imprime ventana permitiendo salir con los puntos.
                     optionReturned = between_answer(1, points_until_now)
                 else:
+                    #Si no cambió categoría, imprime ventana indicando los puntos hasta ahora.
                     optionReturned = between_answer(0, points_until_now)
             
-            if (optionReturned == 1):
+            
+            if (optionReturned == 1): #En caso de que se rinda, se acaba el juego
                 game_over = True
                 #Ventana de fin del juego por rendirse
-                print("Opción 1")
+                #print("Opción 1")
                 end_game(points_until_now, 1, question_count)
                 saveUser(points_until_now)
                 break
-            elif (optionReturned == 2):
+            elif (optionReturned == 2):     #En caso de que se retire, se acaba el juego.
                 game_over = True
                 #Ventana de fin del juego por retirarse
-                print("Opción 2")
+                #print("Opción 2")
                 end_game(points_until_now, 3, question_count)
                 saveUser(points_until_now)
                 break
-            elif (optionReturned == 3):
-                #Ventana de fin de juego por contestar todas las preguntas
-                print("Opción  3")
             
         continue
     
--- MyFrames.py
+++ MyFrames.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
+
+#Clase centrada en la creación de ventanas con una nueva pregunta
 class new_question:
 
     Question = ""
@@ -10,6 +12,7 @@ class new_question:
 
     def __init__(self, question, category, counter):
         self.Question = question
+        #Creación de la ventana
         root = Tk()
         root.title(f"Sofka U - Ronda {category}, Pregunta {counter}")
         root.iconbitmap("Icons/SofkaU_Icon.ico")  
@@ -23,15 +26,16 @@ class new_question:
         labelImg = Label(root, image=img)
         labelImg.place(x=250, y=10)
 
-        varOption = IntVar()
+        varOption = IntVar()    #RadioButtons según las opciones almacenadas en el archivo Preguntas.txt
         optionOne = Radiobutton(root, text=self.saving_answer(self.Question[1].strip(), 1), variable=varOption, value=1, font=20)
         optionTwo = Radiobutton(root, text=self.saving_answer(self.Question[2].strip(), 2), variable=varOption, value=2, font=20)
         optionThree = Radiobutton(root, text=self.saving_answer(self.Question[3].strip(), 3), variable=varOption, value=3, font=20)
         optionFour = Radiobutton(root, text=self.saving_answer(self.Question[4].strip(), 4), variable=varOption, value=4, font=20)
         
-        
+        #Pregunta enviada desde MainRoot
         QuestionLabel = Label(root, text=f"¿{self.Question[0]}", font=20)
         
+        #Solo un decorador, para que las preguntas más largas intenten estar lo más centradas posible.
         if (len(self.Question[0]) <= 60):
             QuestionLabel.place(x=130, y=180)
         elif (len(self.Question[0]) > 60) and (len(self.Question[0]) <= 70):
@@ -47,7 +51,7 @@ class new_question:
         optionFour.place(x=150, y=320)
 
         
-
+        #Función encargada de almacenar la intención del jugador por no contestar la pregunta
         def Exit_window():
             self.wants_to_exit = True
             self.points = 0
@@ -55,14 +59,15 @@ class new_question:
         MyButtonClose = Button(root, text="Salir", padx=10, pady=10, command=Exit_window, font=("", 10, "bold"), borderwidth=2, relief="solid")
         MyButtonClose.place(x=400, y=375)
 
+        #Función dedicada a tomar la respuesta elegida por el usuario y verificar que sea correcta
         def answer_question():
-            if (varOption.get() != self.right_answer):
-                print("Perdió!")
+            if (varOption.get() != self.right_answer): #Si es incorrecta, gameover
+                #print("Perdió!")
                 self.game_over = True
                 self.points = 0
                 root.destroy()
-            else:
-                print("Acertó!")
+            else:   #Si es correcta, sigue en el juego.
+                #print("Acertó!")
                 self.game_over = False
                 self.points = 4
                 root.destroy()
@@ -71,6 +76,7 @@ class new_question:
 
         root.mainloop()
 
+    #Toma las opciones disponibles para la pregunta, y almacena la correcta para poder evaluarla
     def saving_answer(self, question, number):
         if ("!" in question):
             question = question.replace("!", "")
@@ -79,13 +85,13 @@ class new_question:
         else:
             return question
 
+#Primera ventana que el jugador ve. El mensaje se imprime a partir del archivo Introduccion.txt
 class first_window:
 
     closed = False
 
     def __init__(self):
-
-        
+        #Creación de la ventana        
         root = Tk()
         root.title("Preguntas y Respuestas - Sofka U")
         root.iconbitmap("Icons/SofkaU_Icon.ico")  
@@ -96,7 +102,7 @@ class first_window:
 
         LabelTittle = Label(root, text="Trivia - ¿Quién quiere ser Sofkiano?", font=("", 20, "bold"))
         LabelTittle.place(x=80, y=25)
-
+        #Si presiona el botón "empezar", significa que quiere empezar a jugar, y empieza el juego.
         def returnResponse():
             self.closed = True
             root.destroy()
@@ -104,7 +110,7 @@ class first_window:
         MyButtonStart = Button(root, text="Empezar", padx=10, pady=5, command=returnResponse, font=("", 10, "bold"), borderwidth=2, relief="solid")
         MyButtonStart.place(x=550, y=400)
 
-        
+        #Si presiona el boton "Salir", simplemente se cerrará la ventana y no empezará el juego.
         def close_window():
             self.closed = False
             root.destroy()
@@ -113,6 +119,8 @@ class first_window:
 
         root.mainloop()
 
+#Clase centrada en crear las ventanas entre una pregunta y otra, 
+#permitiendole al jugador retirarse o no con sus puntos según los parámetros de entrada
 class between_right_answer:
 
     Retreat = False
@@ -120,9 +128,7 @@ class between_right_answer:
     Surrender = False
 
     def __init__(self, option, points):
-        
-
-
+        #Creación de la ventana
         root = Tk()
         root.title("Sofka U - Correcto!")
         root.iconbitmap("Icons/SofkaU_Icon.ico")
@@ -136,18 +142,20 @@ class between_right_answer:
         LabelOne = Label(root, text="Felicidades, ha contestado correctamente!", font=("", 15, "bold"))
         LabelOne.place(x=130, y=175)
 
+        #Función que almacena la intención del jugador por retirarse con sus puntos.
         def retreat_button():
             self.Retreat = True
             root.destroy()
-            
+        #Función que almacena la intención del jugador por responder otra pregunta.
         def next_question():
             self.Next_question = True
             root.destroy()
-
+        #Función que almacena la intención del jugador por acabar el juego sin sus puntos.
         def surrender():
             self.Surrender = True
             root.destroy()
-
+        
+        #Si la última pregunta contestada fue final de categoría, permite conservar sus puntos
         if (option == 1):
             LabelTwo = Label(root, text="Puede retirarse ahora conservando su total de puntos", font=20)
             LabelThree = Label(root, text="O puede continuar a la siguiente pregunta", font=20)
@@ -155,6 +163,7 @@ class between_right_answer:
             ButtonOne.place(x=450, y=400)
             ButtonTwo = Button(root, text="Siguiente", padx=10, pady=10, command=lambda:next_question(), font=("", 10, "bold"), borderwidth=2, relief="solid")
             ButtonTwo.place(x=550, y=400)
+        #Si la última pregunta contestada no era final de categoría, solo permite avanzar.
         else:
             LabelTwo = Label(root, text="Puede retirarse sin conservar sus puntos", font=20)
             LabelThree = Label(root, text="O puede continuar a la siguiente pregunta", font=20)
@@ -169,31 +178,34 @@ class between_right_answer:
 
         root.mainloop()
 
+#Clase centrada en crear las ventanas de fin de juego, indicando si ganó o perdió, y las preguntas contestadas
 class endgame:
 
-
     def __init__(self, end, points, answered_questions):
+        #Creación de la ventana
         root = Tk()
         root.title("Sofka U - Fin del juego")
         root.iconbitmap("Icons/SofkaU_Icon.ico")
         root.geometry("650x450")
         root.resizable(False, False)
-
+        
         def close_window():
             root.destroy()
 
+        #Si el parámetro es uno, significa que ha perdido el juego. Adapta Label
         if (end == 1):
             #Cuando se equivoca en una pregunta
-            print("Game Over")
-
+            #print("Game Over")
             LabelOne = Label(root, text="Has perdido el juego :(", font=("", 30, "bold"))
             LabelOne.place(x=100, y=50)            
 
+        #Si el parámetro es 2, significa que ha contestado correctamente las 25 preguntas. Adapta Label
         elif (end == 2):
             #Cuando completa las 25 preguntas
-            print("Ganador indiscutible")
+            #print("Ganador indiscutible")
             LabelOne = Label(root, text="Has contestado correctamente las 25 preguntas :D", font=("", 20, "bold"))
             LabelOne.place(x=20, y=50)
+        #Si el parámetro es 3, significa que se retira del juego.
         elif (end == 3):
             #Cuando se retira
             LabelOne = Label(root, text=f"Has contestado correctamente {answered_questions} preguntas :D", font=("", 20, "bold"))
@@ -211,6 +223,7 @@ class endgame:
 
         root.mainloop()
 
+#Clase centrada en guardar a los usuarios en un archivo local, según sea el caso.
 class save_user:
 
     username = ""
@@ -231,9 +244,9 @@ class save_user:
         img = PhotoImage(file=("SofkaU_Logo.png"))
         labelImg = Label(root, image=img)
         labelImg.place(x=250, y=80)
-
+        #Almacena los datos del usuario 
         def save_data_user():
-            print("Guardar usuario")
+            #print("Guardar usuario")
             self.username = usernameText.get()
             self.points = points
             root.destroy()
@@ -254,15 +267,16 @@ class save_user:
 
         root.mainloop()
 
+    #Almacena al jugador en el archivo Historico.txt
     def save_username(self):
         
         #print(self.username, "<--->", self.points)
         
         with open("Historico_Jugadores/Historico.txt", "a+", encoding="utf-8") as registro:
-            
+            #Lee todas las lineas del archivo Historico.txt y las almacena en forma de lista.
             usuarios = registro.readlines()
 
-
+            #Un decorador para que todos tengan la misma longitud
             if (len(self.username) > 10):
                 usuarios.append(f"Usuario: {self.username[:10]}, \tPuntos: {self.points}\n")
             else:
@@ -272,17 +286,16 @@ class save_user:
                     else:
                         break
                 usuarios.append(f"Usuario: {self.username[:10]}, \tPuntos: {self.points}\n")
-            
+            #Luego de agregar el jugador nuevo, sobreescribe el archivo Historico.txt 
             for usuario in usuarios:
                 registro.write(usuario)
     
+    #Imprime todos los jugadores almacenados en el archivo Historico.txt
     def print_users(self):
         with open("Historico_Jugadores/Historico.txt", "r", encoding="utf-8") as registro:
             users = ""
             for line in registro.readlines():
                 users += line.replace("{", "").replace("}", "").strip() + "\n"
-
-          
             
             root = Tk()
             root.title("Sofka U - Registro de Jugadores")
